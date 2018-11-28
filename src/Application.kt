ce71ac912e17b886b0881b01f7d6ca3afe9c31bd
Alex Hall <james_a_hall@homedepot.com>
2018-11-28 13:45:30 -0500

    Add hello world routes

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,31 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
 }
 
